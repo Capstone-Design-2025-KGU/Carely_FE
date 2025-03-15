03838eaace45193b50d6deb2f2ddbd736f220a03
boroboro01 <duke7201@naver.com>
2025-03-15 18:40:01 +0900

    feat: add scroll-controller to term-screen(for UX)

--- lib/widgets/default_button.dart
+++ lib/widgets/default_button.dart
@@ -5,17 +5,19 @@ import 'package:flutter/material.dart';
 class DefaultButton extends StatelessWidget {
   final String content;
   final VoidCallback onPressed;
+  final bool isEnable;
 
   const DefaultButton({
     super.key,
     required this.content,
     required this.onPressed,
+    this.isEnable = true,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: onPressed,
+      onPressed: isEnable ? onPressed : null,
       style: TextButton.styleFrom(
         minimumSize: Size(
           ScreenSize.width(context, 336),
@@ -23,7 +25,7 @@ class DefaultButton extends StatelessWidget {
         ),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
         foregroundColor: Colors.white,
-        backgroundColor: AppColors.mainPrimary,
+        backgroundColor: isEnable ? AppColors.mainPrimary : AppColors.gray100,
         textStyle: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
       ),
       child: Text(content),
