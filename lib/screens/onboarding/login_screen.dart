3b846a5b19d84d880189cf9de6461e20609e145b
boroboro01 <duke7201@naver.com>
2025-03-15 17:11:58 +0900

    feat: add navigator to term-screen from login-screen

--- lib/screens/onboarding/login_screen.dart
+++ lib/screens/onboarding/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carely/screens/onboarding/term_screen.dart';
 import 'package:carely/widgets/default_button.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +14,16 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: DefaultButton(content: '시작하기', onPressed: () {})),
+      body: Center(
+        child: DefaultButton(
+          content: '시작하기',
+          onPressed: () {
+            Navigator.of(
+              context,
+            ).push(MaterialPageRoute(builder: (context) => const TermScreen()));
+          },
+        ),
+      ),
     );
   }
 }
