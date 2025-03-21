0d522679eb75c16a157c972951813d4245925362
boroboro01 <duke7201@naver.com>
2025-03-21 15:36:56 +0900

    feat: add time-stamp to chat-bubble

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -24,13 +24,57 @@ class _ChatScreenState extends State<ChatScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
             child: Column(
               children: [
-                ChatBubble(content: '안녕하십니까?', isMine: true),
+                ChatTimeStamp(timeStamp: '2025년 03월 21일'),
+                ChatBubble(
+                  content: '안녕하십니까?',
+                  isMine: true,
+                  timeStamp: '18:57',
+                ),
                 ChatBubble(
                   content: '안녕하십니까?안녕하십니까?안녕하십니까?안녕하십니까?안녕하십니까?',
                   isMine: false,
+                  timeStamp: '19:00',
                 ),
-                ChatBubble(content: 'Stand By Me', isMine: true),
+                ChatBubble(
+                  content: 'Stand By Me',
+                  isMine: true,
+                  timeStamp: '20:10',
+                ),
               ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ChatTimeStamp extends StatelessWidget {
+  final String timeStamp;
+  const ChatTimeStamp({super.key, required this.timeStamp});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20.0),
+      child: Align(
+        child: Container(
+          constraints: BoxConstraints(
+            maxWidth: ScreenSize.width(context, 200.0),
+          ),
+          decoration: BoxDecoration(
+            color: AppColors.gray700,
+            borderRadius: BorderRadius.circular(24.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 6.0),
+            child: Text(
+              timeStamp,
+              style: TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: Colors.white,
+              ),
             ),
           ),
         ),
@@ -41,31 +85,58 @@ class _ChatScreenState extends State<ChatScreen> {
 
 class ChatBubble extends StatelessWidget {
   final String content;
+  final String timeStamp;
   final bool isMine;
 
-  const ChatBubble({super.key, required this.content, required this.isMine});
+  const ChatBubble({
+    super.key,
+    required this.content,
+    required this.isMine,
+    required this.timeStamp,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: isMine ? Alignment.centerRight : Alignment.centerLeft,
-      child: Container(
-        constraints: BoxConstraints(maxWidth: ScreenSize.width(context, 200.0)),
-        decoration: BoxDecoration(
-          color: isMine ? AppColors.mainPrimary : Colors.white,
-          borderRadius: BorderRadius.circular(24.0),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
-          child: Text(
-            content,
-            style: TextStyle(
-              fontSize: 15.0,
-              fontWeight: FontWeight.w400,
-              color: isMine ? Colors.white : AppColors.gray800,
-            ),
+    // Message Bubble
+    final bubble = Container(
+      constraints: BoxConstraints(maxWidth: ScreenSize.width(context, 200.0)),
+      decoration: BoxDecoration(
+        color: isMine ? AppColors.mainPrimary : Colors.white,
+        borderRadius: BorderRadius.circular(24.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+        child: Text(
+          content,
+          style: TextStyle(
+            fontSize: 15.0,
+            fontWeight: FontWeight.w400,
+            color: isMine ? Colors.white : AppColors.gray800,
           ),
         ),
+      ),
+    );
+
+    // Time Stamp
+    final time = Text(
+      timeStamp,
+      style: TextStyle(
+        fontWeight: FontWeight.w400,
+        color: AppColors.gray600,
+        fontSize: 12.0,
+      ),
+    );
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: Row(
+        mainAxisAlignment:
+            isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children:
+            isMine
+                ? [time, SizedBox(width: 6.0), bubble]
+                : [bubble, SizedBox(width: 6.0), time],
       ),
     );
   }
