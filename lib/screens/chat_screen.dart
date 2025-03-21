c7f394392a6d58bbfa2522ce9a6a895a7df37f01
boroboro01 <duke7201@naver.com>
2025-03-21 15:12:47 +0900

    feat: add message alignment to distinguish between sender and receiver

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carely/utils/member_type.dart';
 import 'package:flutter/material.dart';
 
 import 'package:carely/theme/colors.dart';
@@ -19,12 +20,18 @@ class _ChatScreenState extends State<ChatScreen> {
       appBar: DefaultAppBar(title: '( ) 님과의 채팅방'),
       body: Expanded(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              ChatBubble(content: '안녕하십니까?'),
-              ChatBubble(content: '안녕하십니까?안녕하십니까?안녕하십니까?안녕하십니까?안녕하십니까?'),
-              ChatBubble(content: 'Stand By Me'),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              children: [
+                ChatBubble(content: '안녕하십니까?', isMine: true),
+                ChatBubble(
+                  content: '안녕하십니까?안녕하십니까?안녕하십니까?안녕하십니까?안녕하십니까?',
+                  isMine: false,
+                ),
+                ChatBubble(content: 'Stand By Me', isMine: true),
+              ],
+            ),
           ),
         ),
       ),
@@ -33,25 +40,30 @@ class _ChatScreenState extends State<ChatScreen> {
 }
 
 class ChatBubble extends StatelessWidget {
-  String content;
-  ChatBubble({super.key, required this.content});
+  final String content;
+  final bool isMine;
+
+  const ChatBubble({super.key, required this.content, required this.isMine});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints: BoxConstraints(maxWidth: ScreenSize.width(context, 200.0)),
-      decoration: BoxDecoration(
-        color: AppColors.mainPrimary,
-        borderRadius: BorderRadius.circular(24.0),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
-        child: Text(
-          content,
-          style: TextStyle(
-            fontSize: 15.0,
-            fontWeight: FontWeight.w400,
-            color: Colors.white,
+    return Align(
+      alignment: isMine ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        constraints: BoxConstraints(maxWidth: ScreenSize.width(context, 200.0)),
+        decoration: BoxDecoration(
+          color: isMine ? AppColors.mainPrimary : Colors.white,
+          borderRadius: BorderRadius.circular(24.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+          child: Text(
+            content,
+            style: TextStyle(
+              fontSize: 15.0,
+              fontWeight: FontWeight.w400,
+              color: isMine ? Colors.white : AppColors.gray800,
+            ),
           ),
         ),
       ),
