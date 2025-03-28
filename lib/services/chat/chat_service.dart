f0cfe62547a1b368d6fee6153a1e171927e406d8
boroboro01 <duke7201@naver.com>
2025-03-28 13:25:37 +0900

    feat: create API method to get chat-room

--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -1,9 +1,20 @@
 import 'package:carely/models/chat_message.dart';
+import 'package:carely/models/chat_room.dart';
 import 'package:carely/services/api_service.dart';
 
 class ChatService {
   ChatService._();
   static final ChatService instance = ChatService._();
+
+  Future<List<ChatRoom>> fetchChatRoom(int memberId) async {
+    final response = await APIService.instance.request(
+      '/chat/$memberId/rooms',
+      DioMethod.get,
+    );
+
+    final List<dynamic> data = response.data;
+    return data.map((json) => ChatRoom.fromJson(json)).toList();
+  }
 
   Future<List<ChatMessage>> fetchMessages(int chatRoomId) async {
     final response = await APIService.instance.request(
