4383600d1a461f82e7c345c3ca8cf36aac8b4e04
boroboro01 <duke7201@naver.com>
2025-03-28 10:21:13 +0900

    feat: create chat-room widget

--- lib/screens/chat/chat_room_screen.dart
+++ lib/screens/chat/chat_room_screen.dart
@@ -20,7 +20,114 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
           style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
         ),
       ),
-      body: NoChatRoomWidget(),
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 20.0,
+                right: 20.0,
+                top: 24.0,
+                bottom: 32.0,
+              ),
+              child: ListView(
+                children: [
+                  Text(
+                    '이웃 대화',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  ChatRoomCard(),
+                  SizedBox(height: 28.0),
+                  ChatRoomCard(),
+                  SizedBox(height: 28.0),
+                  ChatRoomCard(),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: ListView(
+                children: [
+                  Text(
+                    '이웃 대화',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  ChatRoomCard(),
+                  SizedBox(height: 28.0),
+                  ChatRoomCard(),
+                  SizedBox(height: 28.0),
+                  ChatRoomCard(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ChatRoomCard extends StatelessWidget {
+  const ChatRoomCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: ScreenSize.height(context, 48.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image(image: AssetImage('assets/images/temp-user-image.png')),
+          SizedBox(width: 16.0),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '간병인 이상덕님',
+                      style: TextStyle(
+                        color: AppColors.gray800,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      '오전 7:00',
+                      style: TextStyle(
+                        color: AppColors.gray500,
+                        fontSize: 11.0,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+
+                Text(
+                  '안녕하세요?!',
+                  style: TextStyle(
+                    color: AppColors.gray500,
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -35,7 +142,7 @@ class NoChatRoomWidget extends StatelessWidget {
       children: [
         Center(
           child: Image(
-            image: AssetImage('assets/images/no_chat_room.png'),
+            image: AssetImage('assets/images/no-chat-room.png'),
             width: ScreenSize.width(context, 248.0),
           ),
         ),
