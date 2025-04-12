8c0c0fb99691ffb6908a332bb271a3731fb1e1cd
boroboro01 <duke7201@naver.com>
2025-04-12 10:29:22 +0900

    feat: integrate address api by using kpostal

--- lib/screens/onboarding/address_screen.dart
+++ lib/screens/onboarding/address_screen.dart
@@ -1,3 +1,5 @@
+import 'package:carely/models/address.dart';
+import 'package:carely/providers/member_provider.dart';
 import 'package:carely/screens/onboarding/skill_screen.dart';
 import 'package:carely/widgets/default_app_bar.dart';
 import 'package:carely/widgets/input_select_field.dart';
@@ -5,6 +7,8 @@ import 'package:carely/widgets/input_text_field.dart';
 import 'package:carely/widgets/signup_progress_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:carely/widgets/default_button.dart';
+import 'package:kpostal/kpostal.dart';
+import 'package:provider/provider.dart';
 
 class AddressScreen extends StatefulWidget {
   static String id = 'address-screen';
@@ -32,7 +36,37 @@ class _AddressScreenState extends State<AddressScreen> {
                     InputSelectField(
                       label: '주소',
                       hintText: '거주지를 입력하세요',
-                      onTap: () {},
+                      onTap: () async {
+                        await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder:
+                                (_) => KpostalView(
+                                  callback: (Kpostal res) {
+                                    final fullAddress = res.address; // 도로명 주소
+                                    final latitude = res.latitude;
+                                    final longitude = res.longitude;
+
+                                    // Provider로 저장
+                                    context
+                                        .read<MemberProvider>()
+                                        .updatePartial(
+                                          address: Address(
+                                            province: '', // 필요한 경우 분리
+                                            city: '',
+                                            district: '',
+                                            details: '',
+                                            latitude: latitude ?? 0.0,
+                                            longitude: longitude ?? 0.0,
+                                          ),
+                                        );
+
+                                    // 상태 갱신용 setState 등도 여기서
+                                  },
+                                ),
+                          ),
+                        );
+                      },
                     ),
                     InputTextField(
                       label: '상세 주소',
