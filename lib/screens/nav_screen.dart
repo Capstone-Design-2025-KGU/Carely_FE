148d5fa7c54d0b322062c08dff960aa3fab14cf3
boroboro01 <duke7201@naver.com>
2025-04-26 17:08:06 +0900

    feat: create temp log out button

--- lib/screens/nav_screen.dart
+++ lib/screens/nav_screen.dart
@@ -1,5 +1,6 @@
 import 'package:carely/screens/chat/chat_room_screen.dart';
 import 'package:carely/screens/home_screen.dart';
+import 'package:carely/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:carely/widgets/bottom_nav_bar.dart';
 
@@ -19,7 +20,7 @@ class _NavScreenState extends State<NavScreen> {
     HomeScreen(),
     HomeScreen(),
     ChatRoomScreen(),
-    HomeScreen(),
+    ProfileScreen(),
   ]; // 다른 스크린 파일 생성시 홈 스크린 지우고 새 스크린 추가.
 
   void _onTap(int index) {
