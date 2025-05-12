596445f5980b0b895c44255f5b4719f8a898b7fc
boroboro01 <duke7201@naver.com>
2025-05-13 07:07:37 +0900

    feat: create meeting card

---
+++ lib/screens/memo_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MemoScreen extends StatefulWidget {
+  const MemoScreen({super.key});
+
+  @override
+  State<MemoScreen> createState() => _MemoScreenState();
+}
+
+class _MemoScreenState extends State<MemoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
