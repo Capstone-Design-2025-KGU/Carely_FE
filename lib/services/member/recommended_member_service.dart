83d41151c9930431ba948a72a2c6bfe3dab08705
boroboro01 <duke7201@naver.com>
2025-05-21 10:51:10 +0900

    feat: create recommended member service

---
+++ lib/services/member/recommended_member_service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:dio/dio.dart';
+import 'package:carely/models/recommended_member.dart';
+import 'package:carely/services/api_service.dart';
+
+class RecommendedMemberService {
+  static Future<List<RecommendedMember>> fetchRecommendedMembers(
+    String token,
+  ) async {
+    try {
+      final response = await APIService.instance.request(
+        '/v1/members/recommend',
+        DioMethod.get,
+        token: token,
+      );
+
+      final content = response.data['content'] as List<dynamic>;
+      return content.map((e) => RecommendedMember.fromJson(e)).toList();
+    } catch (e, stack) {
+      print('추천 이웃 요청 실패: $e\n$stack');
+      rethrow;
+    }
+  }
+}
